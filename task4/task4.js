375ab540f8f18905a9f05944bd0d7ecfb5b42cc9
utsav8818 <utsavkhadka8818@gmail.com>
2026-08-24 11:45:02 +0545

    task4 completed

---
+++ task4/task4.js
@@ -0,0 +1,8 @@
+let html = document.querySelector(".title1");
+let css = document.querySelector(".title2");
+let javaScript = document.querySelector(".title3");
+let infoParagraph = document.querySelector(".info_paragraph")
+
+css.addEventListener("click",() =>{
+    
+})
