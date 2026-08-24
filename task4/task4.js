55eaa4dbda1f5c9a323a1bb180fabe4211e1117d
utsav8818 <utsavkhadka8818@gmail.com>
2026-08-24 12:06:52 +0545

    task4 updated

--- task4/task4.js
+++ task4/task4.js
@@ -1,8 +1,28 @@
 let html = document.querySelector(".title1");
 let css = document.querySelector(".title2");
 let javaScript = document.querySelector(".title3");
-let infoParagraph = document.querySelector(".info_paragraph")
+let htmlParagraph = document.querySelector(".html_container");
+let cssParagraph = document.querySelector(".css_container");
+let jsParagraph = document.querySelector(".js_container");
+
+html.addEventListener("click",() =>{
+    htmlParagraph.style.display = "inline";
+    cssParagraph.style.display = "none";
+    jsParagraph.style.display = "none";
+    
+})
 
 css.addEventListener("click",() =>{
+    console.log(htmlParagraph)
+    htmlParagraph.style.display = "none";
+    cssParagraph.style.display = "inline";
+    jsParagraph.style.display = "none";
+    
+})
+
+javaScript.addEventListener("click",() =>{
+    htmlParagraph.style.display = "none";
+    cssParagraph.style.display = "none";
+    jsParagraph.style.display = "inline";
     
 })
